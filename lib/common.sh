d76cf94b18e3b74527b2b5082f0e1dd92ecaf8db
thibault.dewailly <thibault.dewailly@ovh.net>
2016-04-01 09:52:39 +0200

    hardening : building basic configuration

--- lib/common.sh
+++ lib/common.sh
@@ -2,6 +2,6 @@
 
 logger() {
     test -z "$SCRIPT_NAME" && SCRIPT_NAME=$(basename $0)
-    logger -i -t "$SCRIPT_NAME" -p "user.info" "$(date +%Y.%m.%d-%H:%M:%S) $*"
-    test -t 1 && echo "$(date +%Z-%Y.%m.%d-%H:%M:%S) $*"
+    /usr/bin/logger -i -t "$SCRIPT_NAME" -p "user.info" "$*"
+    test -t 1 && echo "$*"
 }
--- src/skel.sh
+++ src/skel.sh
@@ -4,19 +4,21 @@
 # CIS Debian 7 Hardening
 #
 
-
 #
 # Hardening script skeleton replace this line with proper point treated
 #
 
-# This function will be called if the script status is ont enabled / audit mode
-audit () {
+set -e # One error, it's over
+set -u # One variable unset, it's over
 
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+    :
 }
 
 # This function will be called if the script status is on enabled mode
 apply () {
-
+    :
 }
 
 # Environment Sanitizing
@@ -34,11 +36,39 @@ else
     fi
 fi 
 
-SCRIPT_NAME=$(basename $0)
+LONG_SCRIPT_NAME=$(basename $0)
+SCRIPT_NAME=${LONG_SCRIPT_NAME%.sh}
+# Variable initialization, to avoid crash
+status=""
+params=""
 
+[ -r $CIS_ROOT_DIR/lib/constants.sh ] && . $CIS_ROOT_DIR/lib/constants.sh
+[ -r $CIS_ROOT_DIR/lib/utils.sh     ] && . $CIS_ROOT_DIR/lib/utils.sh
+[ -r $CIS_ROOT_DIR/lib/common.sh    ] && . $CIS_ROOT_DIR/lib/common.sh
+[ -r $CIS_ROOT_DIR/etc/hardening.cfg ] && . $CIS_ROOT_DIR/etc/hardening.cfg
 # Source general configuration file and Specific configuration file if exist
 
-[ -r $ROOT_DIR/etc/hardening.cfg ] && . $ROOT_DIR/etc/hardening.cfg
-[ -r $ROOT_DIR/etc/hardening/$SCRIPT_NAME ] && . $ROOT_DIR/etc/hardening/$SCRIPT_NAME
+[ -r $CIS_ROOT_DIR/etc/conf.d/$SCRIPT_NAME.cfg ] && . $CIS_ROOT_DIR/etc/conf.d/$SCRIPT_NAME.cfg
 
+logger "Working on $SCRIPT_NAME"
 
+if [ -z $status ]; then
+    logger "Could not find status variable for $SCRIPT_NAME, considered as disabled"
+    exit 0
+fi
+
+case $status in
+    enabled | true ) 
+        audit $params # Perform audit
+        apply $params # Perform hardening
+        ;;
+    audit )
+        audit $params # Perform audit
+        ;;
+    disabled | false )
+        logger "$SCRIPT_NAME is disabled, ignoring"
+        ;;
+    *)
+        logger "Wrong value for status : $status. Must be [ enabled | true | audit | disabled | false ]"
+        ;;
+esac

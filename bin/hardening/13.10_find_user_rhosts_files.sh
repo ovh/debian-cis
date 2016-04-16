39e9c794e47674e71273ffa50644220049fb54d4
thibault.dewailly <thibault.dewailly@ovh.net>
2016-04-16 18:55:44 +0200

    13.10_find_user_rhosts_files.sh

--- bin/hardening/13.10_find_user_rhosts_files.sh
+++ bin/hardening/13.10_find_user_rhosts_files.sh
@@ -6,54 +6,34 @@
 #
 
 #
-# 13.9 Check Permissions on User .netrc Files (Scored)
+# 13.10 Check for Presence of User .rhosts Files (Scored)
 #
 
 set -e # One error, it's over
 set -u # One variable unset, it's over
 
-PERMISSIONS="600"
 ERRORS=0
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
     for DIR in $(cat /etc/passwd | egrep -v '(root|halt|sync|shutdown)' | awk -F: '($7 != "/usr/sbin/nologin" && $7 != "/bin/false" && $7 !="/nonexistent" ) { print $6 }'); do
     debug "Working on $DIR"
-        for FILE in $DIR/.netrc; do
+        for FILE in $DIR/.rhosts; do
             if [ ! -h "$FILE" -a -f "$FILE" ]; then
-                has_file_correct_permissions $FILE $PERMISSIONS
-                if [ $FNRET = 0 ]; then
-                    ok "$FILE has correct permissions"
-                else
-                    crit "$FILE has not $PERMISSIONS permissions set"
-                    ERRORS=$((ERRORS+1))
-                fi
+                crit "$FILE present"
+                ERRORS=$((ERRORS+1))
             fi
         done
     done
 
     if [ $ERRORS = 0 ]; then
-        ok "permission $PERMISSIONS set on .netrc users files"
-    fi
-
+        ok "No .rhosts present in users files"
+    fi 
 }
 
 # This function will be called if the script status is on enabled mode
 apply () {
-    for DIR in $(cat /etc/passwd | egrep -v '(root|halt|sync|shutdown)' | awk -F: '($7 != "/usr/sbin/nologin" && $7 != "/bin/false" && $7 !="/nonexistent" ) { print $6 }'); do
-    debug "Working on $DIR"
-        for FILE in $DIR/.netrc; do
-            if [ ! -h "$FILE" -a -f "$FILE" ]; then
-                has_file_correct_permissions $FILE $PERMISSIONS
-                if [ $FNRET = 0 ]; then
-                    ok "$FILE has correct permissions"
-                else
-                    warn "$FILE has not $PERMISSIONS permissions set"
-                    chmod 600 $FILE
-                fi
-            fi
-        done
-    done
+    info "If the audit returns something, please check with the user why he has this file"
 }
 
 # This function will check config parameters required

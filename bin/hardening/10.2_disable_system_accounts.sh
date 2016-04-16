c193bd49f52ca88c2b53902ea620a637910c8739
thibault.dewailly <thibault.dewailly@ovh.net>
2016-04-16 12:57:24 +0200

    12.11_find_sgid_files.sh

--- bin/hardening/10.2_disable_system_accounts.sh
+++ bin/hardening/10.2_disable_system_accounts.sh
@@ -19,7 +19,20 @@ RESULT=''
 # This function will be called if the script status is on enabled / audit mode
 audit () {
     info "Checking if admin accounts have login different from $SHELL"
-    eval $(RESULT=$(egrep -v "^\+" $FILE | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/usr/sbin/nologin" && $7!="/bin/false") {print}' | grep -v "$EXCEPTIONS"))
+    RESULT=$(egrep -v "^\+" $FILE | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/usr/sbin/nologin" && $7!="/bin/false") {print}') 
+    for LINE in $RESULT; do
+        debug "line : $LINE"
+        ACCOUNT=$( echo $LINE | cut -d: -f 1 )
+        debug "Account : $ACCOUNT"
+        debug "Exceptions : $EXCEPTIONS"
+        debug "echo \"$EXCEPTIONS\" | grep -q $ACCOUNT"
+        if echo "$EXCEPTIONS" | grep -q $ACCOUNT; then
+            debug "$ACCOUNT is confirmed as an exception"
+            RESULT=$(sed "s!$LINE!!" <<< "$RESULT")
+        else
+            debug "$ACCOUNT not found in exceptions" 
+        fi
+    done
     if [ ! -z "$RESULT" ]; then
         crit "Some admin accounts have not $SHELL as shell"
         crit "$RESULT"
@@ -30,7 +43,20 @@ audit () {
 
 # This function will be called if the script status is on enabled mode
 apply () {
-    eval $(RESULT=$(egrep -v "^\+" $FILE | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/usr/sbin/nologin" && $7!="/bin/false") {print}' | grep -v "$EXCEPTIONS"))
+    RESULT=$(egrep -v "^\+" $FILE | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/usr/sbin/nologin" && $7!="/bin/false") {print}')
+    for LINE in $RESULT; do
+        debug "line : $LINE"
+        ACCOUNT=$( echo $LINE | cut -d: -f 1 )
+        debug "Account : $ACCOUNT"
+        debug "Exceptions : $EXCEPTIONS"
+        debug "echo \"$EXCEPTIONS\" | grep -q $ACCOUNT"
+        if echo "$EXCEPTIONS" | grep -q $ACCOUNT; then
+            debug "$ACCOUNT is confirmed as an exception"
+            RESULT=$(sed "s!$LINE!!" <<< "$RESULT")
+        else
+            debug "$ACCOUNT not found in exceptions"
+        fi
+    done
     if [ ! -z "$RESULT" ]; then
         warn "Some admin accounts have not $SHELL as shell"
         warn "$RESULT"
@@ -45,7 +71,7 @@ apply () {
 
 # This function will check config parameters required
 check_config() {
-    if [ -z $EXCEPTIONS ]; then
+    if [ -z "$EXCEPTIONS" ]; then
         EXCEPTIONS="@"
     fi
 }

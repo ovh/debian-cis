aad764bb1b133c16106171b51b9b09de584dfa3b
thibault.dewailly <thibault.dewailly@ovh.net>
2016-04-17 19:53:47 +0200

    13.14_check_duplicate_uid.sh 13.15_check_duplicate_gid.sh^C

--- bin/hardening/13.12_users_valid_homedir.sh
+++ bin/hardening/13.12_users_valid_homedir.sh
@@ -16,7 +16,12 @@ ERRORS=0
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
-    cat /etc/passwd | awk -F: '{ print $1 " " $3 " " $6 }' | while read USER USERID DIR; do
+    RESULT=$(cat /etc/passwd | awk -F: '{ print $1 ":" $3 ":" $6 }')
+    for LINE in $RESULT; do 
+        debug "Working on $LINE"
+        USER=$(awk -F: {'print $1'} <<< $LINE)
+        USERID=$(awk -F: {'print $2'} <<< $LINE)
+        DIR=$(awk -F: {'print $3'} <<< $LINE)
         if [ $USERID -ge 1000 -a ! -d "$DIR" -a $USER != "nfsnobody" -a $USER != "nobody" ]; then
             crit "The home directory ($DIR) of user $USER does not exist."
             ERRORS=$((ERRORS+1))    
@@ -30,10 +35,7 @@ audit () {
 
 # This function will be called if the script status is on enabled mode
 apply () {
-    if [ $ERRORS != 0 ]; then
-        warn "Consider creating missing home directories"
-    fi
-    : 
+    info "Modifying home directories may seriously harm your system, report only here"
 }
 
 # This function will check config parameters required

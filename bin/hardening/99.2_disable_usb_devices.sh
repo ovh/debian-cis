57121f116cd9d29a5d6ee10528da9e76ce4d0c23
thibault.dewailly <thibault.dewailly@ovh.net>
2016-04-18 11:16:05 +0200

    99.1_timeout_tty.sh 99.2_disable_usb_devices.sh

--- bin/hardening/99.2_disable_usb_devices.sh
+++ bin/hardening/99.2_disable_usb_devices.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #
-# CIS Debian 7 Hardening
+# CIS Debian 7 Hardening /!\ Not in the Guide
 # Authors : Thibault Dewailly, OVH <thibault.dewailly@corp.ovh.com>
 #
 
 #
-# 10.4 Set Default umask for Users (Scored)
+# 99.2 Disable USB Devices
 #
 
 set -e # One error, it's over
 set -u # One variable unset, it's over
 
 USER='root'
-PATTERN='umask 077'
-FILES_TO_SEARCH='/etc/bash.bashrc /etc/profile.d/* /etc/profile'
-FILE='/etc/profile.d/CIS_10.4_umask.sh'
+PATTERN='ACTION=="add", SUBSYSTEMS=="usb", TEST=="authorized_default", ATTR{authorized_default}="0"' # We do test disabled by default, whitelist is up to you
+FILES_TO_SEARCH='/etc/udev/rules.d/*'
+FILE='/etc/udev/rules.d/10-CIS_99.2_usb_devices.sh'
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
@@ -34,7 +34,19 @@ apply () {
         warn "$PATTERN not present in $FILES_TO_SEARCH"
         touch $FILE
         chmod 644 $FILE
-        add_end_of_file $FILE "$PATTERN"
+        add_end_of_file $FILE '
+# By default, disable all.
+ACTION=="add", SUBSYSTEMS=="usb", TEST=="authorized_default", ATTR{authorized_default}="0"
+
+# Enable hub devices.
+ACTION=="add", ATTR{bDeviceClass}=="09", TEST=="authorized", ATTR{authorized}="1"
+
+# Enables keyboard devices
+ACTION=="add", ATTR{product}=="*[Kk]eyboard*", TEST=="authorized", ATTR{authorized}="1"
+
+# PS2-USB converter
+ACTION=="add", ATTR{product}=="*Thinnet TM*", TEST=="authorized", ATTR{authorized}="1"
+'
     else
         ok "$PATTERN present in $FILES_TO_SEARCH"
     fi

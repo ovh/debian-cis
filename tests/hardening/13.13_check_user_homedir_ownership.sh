3d0b49a4f937ddd3691f4682b821eac39cd647df
Charles Herlin <charles.herlin@corp.ovh.com>
2019-02-22 15:22:58 +0100

    IMP(13.13): Add exceptions for home directories not owned by owner
    Fill tests
    
    Apply shellcheck recommendations

--- tests/hardening/13.13_check_user_homedir_ownership.sh
+++ tests/hardening/13.13_check_user_homedir_ownership.sh
@@ -2,9 +2,23 @@
 test_audit() {
     describe Running on blank host
     register_test retvalshouldbe 0
-    dismiss_count_for_test
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    useradd -m testhomeuser
+    chown root:root /home/testhomeuser
+
+    describe Wrong home owner
+    register_test retvalshouldbe 1
+    run wronghomeowner /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    echo "EXCEPTIONS=\"/home/testhomeuser:testhomeuser:root\"" >> /opt/debian-cis/etc/conf.d/"${script}".cfg
+    sed -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+
+    describe Added exceptions
+    register_test retvalshouldbe 0
+    run exceptions /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    # Cleanup
+    userdel testhomeuser
 }

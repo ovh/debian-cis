ovh/debian-cis

commit d2bbf754ac967858ccb7a2541588d6b882a2ae29

Migrate generic checks from secaudit to cis-hardening
new file:   99.3.1_acc_shadow_sha512.sh
new file:   99.3.2_acc_sudoers_no_all.sh
new file:   99.4_net_fw_default_policy_drop.sh
new file:   99.5.1_ssh_auth_pubk_only.sh
new file:   99.5.2.1_ssh_cry_kex.sh
new file:   99.5.2.2_ssh_cry_mac.sh
new file:   99.5.2.3_ssh_cry_rekey.sh
new file:   99.5.3_ssh_disable_features.sh
new file:   99.5.4_ssh_keys_from.sh
new file:   99.5.5_ssh_strict_modes.sh
new file:   99.5.6_ssh_sys_accept_env.sh
new file:   99.5.7_ssh_sys_no_legacy.sh
new file:   99.5.8_ssh_sys_sandbox.sh
new file:   99.5.9_ssh_log_level.sh

Fix descriptions in comment section for 99.* secaudit checks

Remove duplicated legacy services that are already taken care of by vanilla cis

Enable custom configuration of checks in config-file, no more hard coded conf
Add test to disable check if debian version is too old
Add excused IPs while checking "from" field of authorized_keys
Escaping dots in IPs
Manage Kex for different debian versions
Add tests for generic checks and add apply for ssh config
Apply shellcheck recommendations on audit/hardening scripts
Update script to check for allowed IPs only, remove bastion related
Fill `apply` func for ssh config related scripts
Add and update tests scenarii

Disable shellcheck test for external source 1091

As of today, the entire project is not shellcheck compliant, I prefer
disabling the test that warns about not finding external source (that
arent compliant). I will enable it again when the project library will
be shellchecked
https://github.com/koalaman/shellcheck/wiki/SC1091

Refactor password policy check with one check by feature

Previous file will now only look for bad passwords in /etc/shadow
I added two checks that look for the compliant configuration lines in
conf files /etc/logins.defs and /etc/pam.d/common-passwords

FIX: merge chained sed and fix regex

FIX: update regex to capture more output
FIX: fix pattern to ignore commented lines, add apply

lib/utils.sh
@@ -95,14 +95,25 @@ has_file_correct_permissions() {
     fi 
 }
 
+does_pattern_exist_in_file_nocase() {
+    _does_pattern_exist_in_file "-Ei" $*
+}
+
 does_pattern_exist_in_file() {
-    local FILE=$1
-    local PATTERN=$2
+    _does_pattern_exist_in_file "-E" $*
+}
+
+_does_pattern_exist_in_file() {
+    local OPTIONS="$1"
+    shift
+    local FILE="$1"
+    shift
+    local PATTERN="$*"
 
     debug "Checking if $PATTERN is present in $FILE"
     if $SUDO_CMD [ -r "$FILE" ] ; then
-        debug "$SUDO_CMD grep -qE -- '$PATTERN' $FILE"
-        if $($SUDO_CMD grep -qE -- "$PATTERN" $FILE); then
+        debug "$SUDO_CMD grep -q $OPTIONS -- '$PATTERN' $FILE"
+        if $($SUDO_CMD grep -q $OPTIONS -- "$PATTERN" $FILE); then
             FNRET=0
         else
             FNRET=1
@@ -382,3 +393,18 @@ is_pkg_installed()
         FNRET=1
     fi
 }
+
+
+# Returns Debian major version
+
+get_debian_major_version()
+{
+    DEB_MAJ_VER=""
+    does_file_exist /etc/debian_version
+    if [ $FNRET ]; then
+        DEB_MAJ_VER=$(cut -d '.' -f1 /etc/debian_version)
+    else
+        DEB_MAJ_VER=$(lsb_release -r | cut -f2 | cut -d '.' -f 1)
+    fi
+}
+

bin/hardening/99.3.1_acc_shadow_sha512.sh
@@ -1,0 +1,79 @@
+#!/bin/bash
+# run-shellcheck
+#
+# OVH Security audit
+#
+
+#
+# Check that any password that may exist in /etc/shadow is SHA512 hashed and salted
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+# shellcheck disable=2034
+DESCRIPTION="Check that any password that may exist in /etc/shadow is SHA512 hashed and salted"
+FILE="/etc/shadow"
+
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+# Review shadow file for existing passwords
+    pw_found=""
+    users_reviewed=""
+    if $SUDO_CMD [ ! -r "$FILE" ]; then
+        crit "$FILE is not readable"
+        return
+    fi
+    for line in $($SUDO_CMD cut -d ":" -f 1,2 /etc/shadow); do
+        users_reviewed+="$line "
+        user=$(echo "$line" | cut -d ":" -f 1)
+        passwd=$(echo "$line" | cut -d ":" -f 2)
+        if [[ $passwd = '!' || $passwd = '*' ]]; then
+            continue
+        # Check password against $6$<salt>$<encrypted>, see `man 3 crypt`
+        elif [[ $passwd =~ ^\$6\$[a-zA-Z0-9./]{2,16}\$[a-zA-Z0-9./]{86}$ ]]; then
+            pw_found+="$user "
+            ok "User $user has suitable SHA512 hashed password."
+        else
+            pw_found+="$user "
+            crit "User $user has a password that is not SHA512 hashed."
+        fi
+    done
+    if [[ -z "$users_reviewed" ]]; then
+        crit "No users were reviewed in $FILE !"
+        return
+    fi
+    if [[ -z "$pw_found" ]]; then
+        ok "There is no password in $FILE"
+    fi
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    :
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi

bin/hardening/99.3.2_acc_sudoers_no_all.sh
@@ -1,0 +1,99 @@
+#!/bin/bash
+# run-shellcheck
+#
+# OVH Security audit
+#
+
+#
+# Checks there are no carte-blanche authorization in sudoers file(s).
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+# shellcheck disable=2034
+DESCRIPTION="Checks there are no carte-blanche authorization in sudoers file(s)."
+
+FILE="/etc/sudoers"
+DIRECTORY="/etc/sudoers.d"
+# spaces will be expanded to [:space:]* when using the regex
+# improves readability in audit report
+REGEX="ALL = \( ALL( : ALL)? \)( NOPASSWD:)? ALL"
+EXCEPT=""
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+    FILES=""
+    if $SUDO_CMD [ ! -r "$FILE" ]; then
+        crit "$FILE is not readable"
+        return
+    fi
+    FILES="$FILE"
+    if $SUDO_CMD [ ! -d "$DIRECTORY" ]; then
+        debug "$DIRECTORY does not exist"
+    elif $SUDO_CMD [ ! -x "$DIRECTORY" ]; then
+        crit "Cannot browse $DIRECTORY"
+    else
+        FILES="$FILES $($SUDO_CMD ls -1 $DIRECTORY | sed s=^=$DIRECTORY/= )"
+    fi
+    for file in $FILES; do
+        if $SUDO_CMD [ ! -r "$file" ]; then
+            crit "$file is not readable"
+        else
+            # shellcheck disable=2001
+            if ! $SUDO_CMD grep -E "$(echo "$REGEX" | sed 's/ /[[:space:]]*/g')" "$file" &> /dev/null ; then
+                ok  "There is no carte-blanche sudo permission in $file"
+            else
+                # shellcheck disable=2001
+                RET=$($SUDO_CMD grep -E "$(echo "$REGEX" | sed 's/ /[[:space:]]*/g')" "$file" | sed 's/\t/#/g;s/ /#/g' )
+                for line in $RET; do
+                    if grep -q "$(echo "$line" | cut -d '#' -f 1)" <<< "$EXCEPT" ; then
+                        # shellcheck disable=2001
+                        ok "$(echo "$line" | sed 's/#/ /g') is present in $file but was EXCUSED because $(echo "$line" | cut -d '#' -f 1) is part of exceptions."
+                        continue
+                    fi
+                    # shellcheck disable=2001
+                    crit "$(echo "$line" | sed 's/#/ /g') is present in $file"
+                done
+            fi
+        fi
+    done
+
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    :
+}
+
+# This function will create the config file for this check with default values
+create_config() {
+    cat <<EOF
+status=disabled
+# Put EXCEPTION account names here, space separated
+EXCEPT="root %root %sudo %wheel"
+EOF
+}
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi

bin/hardening/99.3.3_acc_pam_sha512.sh
@@ -1,0 +1,62 @@
+#!/bin/bash
+# run-shellcheck
+#
+# OVH Security audit
+#
+
+#
+# Check that any password that may exist in /etc/shadow is SHA512 hashed and salted
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+# shellcheck disable=2034
+DESCRIPTION="Check that any password that may exist in /etc/shadow is SHA512 hashed and salted"
+
+CONF_FILE="/etc/pam.d/common-password"
+CONF_LINE="^\s*password\s.+\s+pam_unix\.so\s+.*sha512"
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+# Check conf file for default SHA512 hash
+    if $SUDO_CMD [ ! -r $CONF_FILE ]; then
+        crit "$CONF_FILE is not readable"
+    else
+        does_pattern_exist_in_file $CONF_FILE "$(sed 's/ /[[:space:]]+/g' <<< "$CONF_LINE")"
+        if [ "$FNRET" = 0 ]; then
+            ok "$CONF_LINE is present in $CONF_FILE"
+        else
+            crit "$CONF_LINE is not present in $CONF_FILE"
+        fi
+    fi
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    :
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi

bin/hardening/99.3.4_acc_logindefs_sha512.sh
@@ -1,0 +1,76 @@
+#!/bin/bash
+# run-shellcheck
+#
+# OVH Security audit
+#
+
+#
+# Check that any password that may exist in /etc/shadow is SHA512 hashed and salted
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+# shellcheck disable=2034
+DESCRIPTION="Check that any password that may exist in /etc/shadow is SHA512 hashed and salted"
+
+CONF_FILE="/etc/login.defs"
+CONF_LINE="ENCRYPT_METHOD SHA512"
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+# Check conf file for default SHA512 hash
+    if $SUDO_CMD [ ! -r $CONF_FILE ]; then
+        crit "$CONF_FILE is not readable"
+    else
+        does_pattern_exist_in_file $CONF_FILE "^ *${CONF_LINE/ /[[:space:]]+}"
+        if [ "$FNRET" = 0 ]; then
+            ok "$CONF_LINE is present in $CONF_FILE"
+        else
+            crit "$CONF_LINE is not present in $CONF_FILE"
+        fi
+    fi
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    does_pattern_exist_in_file $CONF_FILE "^ *${CONF_LINE/ /[[:space:]]+}"
+    if [ "$FNRET" = 0 ]; then
+        ok "$CONF_LINE is present in $CONF_FILE"
+    else
+        warn "$CONF_LINE is not present in $CONF_FILE, adding it"
+        does_pattern_exist_in_file "$CONF_FILE" "^$(echo "$CONF_LINE" | cut -d ' ' -f1)"
+        if [ "$FNRET" != 0 ]; then
+            add_end_of_file $CONF_FILE "$CONF_LINE"
+        else
+            info "Parameter $SSH_PARAM is present but with the wrong value -- Fixing"
+            replace_in_file "$CONF_FILE" "^$(echo "$CONF_LINE" | cut -d ' ' -f1)[[:space:]]*.*" "$CONF_LINE"
+        fi
+        /etc/init.d/ssh reload > /dev/null 2>&1
+    fi
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi
+

bin/hardening/99.4_net_fw_default_policy_drop.sh
@@ -1,0 +1,78 @@
+#!/bin/bash
+# run-shellcheck
+#
+# OVH Security audit
+#
+
+#
+# Check iptables firewall default policy for DROP on INPUT and FORWARD.
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+# shellcheck disable=2034
+HARDENING_LEVEL=2
+# shellcheck disable=2034
+DESCRIPTION="Check iptables firewall default policy for DROP on INPUT and FORWARD."
+
+PACKAGE="iptables"
+FW_CHAINS="INPUT FORWARD"
+FW_POLICY="DROP"
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+    is_pkg_installed $PACKAGE
+    if [ "$FNRET" != 0 ]; then
+        crit "$PACKAGE is not installed!"
+    else
+        ipt=$($SUDO_CMD $PACKAGE -nL  || true )
+        if [[ -z $ipt ]]; then
+            crit "Empty return from $PACKAGE command. Aborting..."
+            return
+        fi
+        for chain in $FW_CHAINS; do
+            regex="Chain $chain \(policy ([A-Z]+)\)"
+            # previous line will capture actual policy
+            if [[ $ipt =~ $regex ]]; then
+                actual_policy=${BASH_REMATCH[1]}
+                if [[ $actual_policy == "$FW_POLICY" ]]; then
+                    ok "Policy correctly set to $FW_POLICY for chain $chain"
+                else
+                    crit "Policy set to $actual_policy for chain $chain, should be ${FW_POLICY}."
+                fi
+            else
+                echo "cant find chain $chain"
+            fi
+        done
+    fi
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    :
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi

bin/hardening/99.5.1_ssh_auth_pubk_only.sh
@@ -1,0 +1,97 @@
+#!/bin/bash
+# run-shellcheck
+
+#
+# OVH Security audit
+#
+
+#
+# Ensure that sshd only allows authentication through public key.
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+# shellcheck disable=2034
+HARDENING_LEVEL=2
+# shellcheck disable=2034
+DESCRIPTION="Ensure that sshd only allows authentication through public key."
+
+PACKAGE='openssh-server'
+FILE='/etc/ssh/sshd_config'
+OPTIONS='PubkeyAuthentication=yes PasswordAuthentication=no KbdInteractiveAuthentication=no KerberosAuthentication=no ChallengeResponseAuthentication=no HostbasedAuthentication=no GSSAPIAuthentication=no GSSAPIKeyExchange=no '
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+    is_pkg_installed $PACKAGE
+    if [ "$FNRET" != 0 ]; then
+        crit "$PACKAGE is not installed!"
+    else
+        ok "$PACKAGE is installed"
+        for SSH_OPTION in $OPTIONS; do
+            SSH_PARAM=$(echo "$SSH_OPTION" | cut -d= -f 1)
+            SSH_VALUE=$(echo "$SSH_OPTION" | cut -d= -f 2)
+            PATTERN="^$SSH_PARAM[[:space:]]+$SSH_VALUE"
+            does_pattern_exist_in_file_nocase $FILE "$PATTERN"
+            if [ "$FNRET" = 0 ]; then
+                ok "$PATTERN is present in $FILE"
+            else
+                crit "$PATTERN is not present in $FILE"
+            fi
+        done
+    fi
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    is_pkg_installed $PACKAGE
+    if [ "$FNRET" = 0 ]; then
+        ok "$PACKAGE is installed"
+    else
+        crit "$PACKAGE is absent, installing it"
+        apt_install $PACKAGE
+    fi
+    for SSH_OPTION in $OPTIONS; do
+            SSH_PARAM=$(echo "$SSH_OPTION" | cut -d= -f 1)
+            SSH_VALUE=$(echo "$SSH_OPTION" | cut -d= -f 2)
+            PATTERN="^$SSH_PARAM[[:space:]]+$SSH_VALUE"
+            does_pattern_exist_in_file_nocase $FILE "$PATTERN"
+            if [ "$FNRET" = 0 ]; then
+                ok "$PATTERN is present in $FILE"
+            else
+                warn "$PATTERN is not present in $FILE, adding it"
+                does_pattern_exist_in_file_nocase $FILE "^$SSH_PARAM"
+                if [ "$FNRET" != 0 ]; then
+                    add_end_of_file $FILE "$SSH_PARAM $SSH_VALUE"
+                else
+                    info "Parameter $SSH_PARAM is present but with the wrong value -- Fixing"
+                    replace_in_file $FILE "^$SSH_PARAM[[:space:]]+.*" "$SSH_PARAM $SSH_VALUE"
+                fi
+                /etc/init.d/ssh reload > /dev/null 2>&1
+            fi
+    done
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi

commit 80a1146af743eac2bc829e5f718958b06ac365f2

IMP(8.2.5): find multiline pattern in files (syslog)
Add func to find pattern in file that spreads over multiple lines
The func will remove commented lines (that begin with '#')
and consider the file as one long line.
Thus, this is not possible to look for pattern at beginning of line
with this func ('^' and '$')

Improved pattern in 8.2.5

Add syslog-ng to installed dependencies in Dockerfiles

Fixed multifile arguments when looking for pattern that got broken
in d2bbf754 due to "nocase" and _does_pattern_exist_in_file wrapper
Please note that you can only look for pattern in ONE FILE at once
Fixed 8.2.5 and 8.3.2 with for loop on files and 'FOUND' flag
You now need to specify each and every file to look for or embed a
'find' command as follow :
`FILES="$SYSLOG_BASEDIR/syslog-ng.conf $(find $SYSLOG_BASEDIR/conf.d/)"`

Improved test files
Applied shellcheck recommendations

bin/hardening/8.2.5_syslog-ng_remote_host.sh
@@ -14,27 +14,40 @@ set -u # One variable unset, it's over
 HARDENING_LEVEL=3
 DESCRIPTION="Configure syslog-ng to send logs to a remote log host."
 
-PATTERN='^destination.*(tcp|udp)[[:space:]]*\([[:space:]]*\".*\"[[:space:]]*\)'
+PATTERN='destination[[:alnum:][:space:]*{]+(tcp|udp)[[:space:]]*\(\"[[:alnum:].]+\".'
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
-    FILES="$SYSLOG_BASEDIR/syslog-ng.conf $SYSLOG_BASEDIR/conf.d/*"
-    does_pattern_exist_in_file "$FILES" "$PATTERN"
-    if [ $FNRET != 0 ]; then
-        crit "$PATTERN is not present in $FILES"
-    else
+    FOUND=0
+    FILES="$SYSLOG_BASEDIR/syslog-ng.conf $(find $SYSLOG_BASEDIR/conf.d/)"
+    for FILE in $FILES; do
+       does_pattern_exist_in_file_multiline "$FILE" "$PATTERN"
+        if [ $FNRET == 0 ]; then
+            FOUND=1
+        fi
+    done
+
+    if [ $FOUND == 1 ]; then
         ok "$PATTERN is present in $FILES"
-    fi 
+    else
+        crit "$PATTERN is not present in $FILES"
+    fi
 }
 
 # This function will be called if the script status is on enabled mode
 apply () {
-    FILES="$SYSLOG_BASEDIR/syslog-ng.conf $SYSLOG_BASEDIR/conf.d/*"
-    does_pattern_exist_in_file "$FILES" "$PATTERN"
-    if [ $FNRET != 0 ]; then
-        crit "$PATTERN is not present in $FILES, please set a remote host to send your logs"
-    else
+    FOUND=0
+    FILES="$SYSLOG_BASEDIR/syslog-ng.conf $(find $SYSLOG_BASEDIR/conf.d/ -type f)"
+    for FILE in $FILES; do
+       does_pattern_exist_in_file_multiline "$FILE" "$PATTERN"
+        if [ $FNRET == 0 ]; then
+            FOUND=1
+        fi
+    done
+    if [ $FOUND == 1 ]; then
         ok "$PATTERN is present in $FILES"
+    else
+        crit "$PATTERN is not present in $FILES, please set a remote host to send your logs"
     fi
 }
 
@@ -48,7 +61,7 @@ EOF
 
 # This function will check config parameters required
 check_config() {
-    :    
+    :
 }
 
 # Source Root Dir Parameter

bin/hardening/8.3.2_tripwire_cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# run-shellcheck
 #
 # CIS Debian Hardening
 #
@@ -11,26 +12,34 @@
 set -e # One error, it's over
 set -u # One variable unset, it's over
 
+# shellcheck disable=2034
 HARDENING_LEVEL=4
+# shellcheck disable=2034
 DESCRIPTION="Implemet periodic execution of file integrity."
 
-FILES='/etc/crontab /etc/cron.d/*'
+FILES="/etc/crontab $(find /etc/cron.d/ -type f)"
 PATTERN='tripwire --check'
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
-    does_pattern_exist_in_file "$FILES" "$PATTERN"
-    if [ $FNRET != 0 ]; then
-        crit "$PATTERN is not present in $FILES"
-    else
+    FOUND=0
+    for FILE in $FILES; do
+        does_pattern_exist_in_file "$FILE" "$PATTERN"
+        if [ "$FNRET" == 0 ]; then
+            FOUND=1
+        fi
+    done
+    if [ $FOUND == 1 ]; then
         ok "$PATTERN is present in $FILES"
+    else
+        crit "$PATTERN is not present in $FILES"
     fi
 }
 
 # This function will be called if the script status is on enabled mode
 apply () {
     does_pattern_exist_in_file "$FILES" "$PATTERN"
-    if [ $FNRET != 0 ]; then
+    if [ "$FNRET" != 0 ]; then
         warn "$PATTERN is not present in $FILES, setting tripwire cron"
         echo "0 10 * * * root /usr/sbin/tripwire --check > /dev/shm/tripwire_check 2>&1 " > /etc/cron.d/CIS_8.3.2_tripwire
     else
@@ -54,8 +63,9 @@ if [ -z "$CIS_ROOT_DIR" ]; then
 fi
 
 # Main function, will call the proper functions given the configuration (audit, enabled, disabled)
-if [ -r $CIS_ROOT_DIR/lib/main.sh ]; then
-    . $CIS_ROOT_DIR/lib/main.sh
+if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
+    # shellcheck source=/opt/debian-cis/lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
 else
     echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
     exit 128

lib/utils.sh
@@ -114,15 +114,42 @@ _does_pattern_exist_in_file() {
     if $SUDO_CMD [ -r "$FILE" ] ; then
         debug "$SUDO_CMD grep -q $OPTIONS -- '$PATTERN' $FILE"
         if $($SUDO_CMD grep -q $OPTIONS -- "$PATTERN" $FILE); then
+            debug "Pattern found in $FILE"
             FNRET=0
         else
+            debug "Pattern NOT found in $FILE"
             FNRET=1
         fi
     else
         debug "File $FILE is not readable!"
         FNRET=2
     fi
+}
 
+# Look for pattern in file that can spread over multiple lines
+# The func will remove commented lines (that begin with '#')
+# and consider the file as one long line.
+# Thus, this is not possible to look for pattern at beginning of line
+# with this func ('^' and '$')
+does_pattern_exist_in_file_multiline() {
+    local FILE="$1"
+    shift
+    local PATTERN="$*"
+
+    debug "Checking if multiline pattern: $PATTERN is present in $FILE"
+    if $SUDO_CMD [ -r "$FILE" ] ; then
+        debug "$SUDO_CMD grep -v '^[[:space:]]*#' $FILE | tr '\n' ' ' | grep -Pq -- "$PATTERN""
+        if $($SUDO_CMD grep -v '^[[:space:]]*#' $FILE | tr '\n' ' ' | grep -Pq -- "$PATTERN" ); then
+            debug "Pattern found in $FILE"
+            FNRET=0
+        else
+            debug "Pattern NOT found in $FILE"
+            FNRET=1
+        fi
+    else
+        debug "File $FILE is not readable!"
+        FNRET=2
+    fi
 }
 
 add_end_of_file() {

tests/docker/Dockerfile.debian10_20181226
@@ -2,7 +2,7 @@ FROM debian:buster-20181226
 
 RUN groupadd -g 500 secaudit && useradd  -u 500 -g 500 -s /bin/bash secaudit && mkdir -m 700 /home/secaudit && chown secaudit:secaudit /home/secaudit
 
-RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y bc openssh-server sudo
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y bc openssh-server sudo syslog-ng
 
 COPY --chown=500:500 . /opt/debian-cis/
 

tests/docker/Dockerfile.debian8
@@ -2,7 +2,7 @@ FROM debian:jessie
 
 RUN groupadd -g 500 secaudit && useradd  -u 500 -g 500 -s /bin/bash secaudit && mkdir -m 700 /home/secaudit && chown secaudit:secaudit /home/secaudit
 
-RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y bc openssh-server sudo
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y bc openssh-server sudo syslog-ng
 
 COPY --chown=500:500 . /opt/debian-cis/
 

tests/docker/Dockerfile.debian9
@@ -2,7 +2,7 @@ FROM debian:stretch
 
 RUN groupadd -g 500 secaudit && useradd  -u 500 -g 500 -s /bin/bash secaudit && mkdir -m 700 /home/secaudit && chown secaudit:secaudit /home/secaudit
 
-RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y bc openssh-server sudo
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y bc openssh-server sudo syslog-ng
 
 COPY --chown=500:500 . /opt/debian-cis/
 

tests/hardening/8.2.5_syslog-ng_remote_host.sh
@@ -1,10 +1,49 @@
 # run-shellcheck
 test_audit() {
+#set -x
+
     describe Running on blank host
-    register_test retvalshouldbe 0
-    dismiss_count_for_test
+    register_test retvalshouldbe 1
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    cp -a /etc/syslog-ng/syslog-ng.conf /tmp/syslog-ng.conf.bak
+
+    echo "destination mySyslog tcp (\"syslog.example.tld\")" >> /etc/syslog-ng/syslog-ng.conf
+    grep syslog.example.tld /etc/syslog-ng/syslog-ng.conf
+
+    describe Checking one line conf
+    register_test retvalshouldbe 0
+    run oneline /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+
+    cp -a /tmp/syslog-ng.conf.bak /etc/syslog-ng/syslog-ng.conf
+    cat >> /etc/syslog-ng/syslog-ng.conf <<EOF
+destination mySyslog {
+    tcp ("syslog.example.tld"),
+    port(1234),
+EOF
+
+    describe Checking mutliline conf
+    register_test retvalshouldbe 0
+    run multiline /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    mv /tmp/syslog-ng.conf.bak /etc/syslog-ng/syslog-ng.conf
+
+    #echo "#Sample conf" >/etc/syslog-ng/conf.d/1_tcp_destination
+    echo "destination mySyslog tcp (\"syslog.example.tld\")" >> /etc/syslog-ng/conf.d/1_tcp_destination
+    cat /etc/syslog-ng/conf.d/1_tcp_destination
+
+
+    describe Checking file in subdirectory
+    register_test retvalshouldbe 0
+    run subfile /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+
+
+    # Cleanup
+    #mv /tmp/syslog-ng.conf.bak /etc/syslog-ng/syslog-ng.conf
+
+    rm /etc/syslog-ng/conf.d/1_tcp_destination
+
 }

tests/hardening/8.3.2_tripwire_cron.sh
@@ -1,10 +1,14 @@
 # run-shellcheck
 test_audit() {
     describe Running on blank host
-    register_test retvalshouldbe 0
-    dismiss_count_for_test
+    register_test retvalshouldbe 1
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    sed -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+    /opt/debian-cis/bin/hardening/"${script}".sh || true
+
+    describe Checking auto resolved state
+    register_test retvalshouldbe 0
+    run resolved /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 }
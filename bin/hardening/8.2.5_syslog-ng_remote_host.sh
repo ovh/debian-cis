fbef8586c082beba24be233d380790e8b943f09c
Charles Herlin <charles.herlin@corp.ovh.com>
2019-02-28 16:41:41 +0100

    FIX(8.2.5): grep: x is a directory

--- bin/hardening/8.2.5_syslog-ng_remote_host.sh
+++ bin/hardening/8.2.5_syslog-ng_remote_host.sh
@@ -19,7 +19,7 @@ PATTERN='destination[[:alnum:][:space:]*{]+(tcp|udp)[[:space:]]*\(\"[[:alnum:].]
 # This function will be called if the script status is on enabled / audit mode
 audit () {
     FOUND=0
-    FILES="$SYSLOG_BASEDIR/syslog-ng.conf $(find $SYSLOG_BASEDIR/conf.d/)"
+    FILES="$SYSLOG_BASEDIR/syslog-ng.conf $(find $SYSLOG_BASEDIR/conf.d/ -type f)"
     for FILE in $FILES; do
        does_pattern_exist_in_file_multiline "$FILE" "$PATTERN"
         if [ $FNRET = 0 ]; then

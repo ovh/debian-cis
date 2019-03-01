9ada868f43050020e2bcc20a2e978bdeddf9e2f4
Charles Herlin <charles.herlin@corp.ovh.com>
2019-03-01 12:12:42 +0100

    IMP(8.2.4): add exceptions in check and apply
    Apply shellcheck recommendations

--- tests/hardening/8.2.4_set_logfile_perm.sh
+++ tests/hardening/8.2.4_set_logfile_perm.sh
@@ -6,5 +6,22 @@ test_audit() {
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+
+    describe Creating failed state
+    touch /var/log/auth.log
+    touch /var/log/kern.log
+    register_test retvalshouldbe 1
+    run failing /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    describe Setting exceptions
+    echo 'EXCEPTIONS=/var/log/auth.log:root:root:600' >> /opt/debian-cis/etc/conf.d/"${script}".cfg
+    register_test retvalshouldbe 1
+    run excepandfail /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    describe Correcting situation
+    sed -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+    /opt/debian-cis/bin/hardening/"${script}".sh || true
+
+    register_test retvalshouldbe 0
+    run resolved /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 }

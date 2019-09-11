c863a013051d5d91b95a3f10414e9b5c07f5c878
Charles Herlin <charles.herlin@corp.ovh.com>
2019-09-11 15:40:00 +0200

    Renum 9.2.x to 5.3.x Pam password settings
    	renamed:    bin/hardening/9.2.1_enable_cracklib.sh -> bin/hardening/5.3.1_enable_cracklib.sh
    	renamed:    bin/hardening/9.2.2_enable_lockout_failed_password.sh -> bin/hardening/5.3.2_enable_lockout_failed_password.sh
    	renamed:    bin/hardening/9.2.3_limit_password_reuse.sh -> bin/hardening/5.3.3_limit_password_reuse.sh
    	renamed:    tests/hardening/9.2.1_enable_cracklib.sh -> tests/hardening/5.3.1_enable_cracklib.sh
    	renamed:    tests/hardening/9.2.3_limit_password_reuse.sh -> tests/hardening/5.3.2_enable_lockout_failed_password.sh
    	renamed:    tests/hardening/9.2.2_enable_lockout_failed_password.sh -> tests/hardening/5.3.3_limit_password_reuse.sh

--- bin/hardening/5.3.1_enable_cracklib.sh
+++ bin/hardening/5.3.1_enable_cracklib.sh
@@ -5,18 +5,22 @@
 #
 
 #
-# 9.2.2 Set Lockout for Failed Password Attempts (Not Scored)
+# 5.3.1 Ensure password creation requirements are configured (Scored)
 #
 
 set -e # One error, it's over
 set -u # One variable unset, it's over
 
-HARDENING_LEVEL=3
-DESCRIPTION="Set lockout for failed password attemps."
+HARDENING_LEVEL=2
+DESCRIPTION="Set password creation requirement parameters using pam.cracklib."
 
-PACKAGE='libpam-modules-bin'
-PATTERN='^auth[[:space:]]*required[[:space:]]*pam_tally[2]?.so'
-FILE='/etc/pam.d/login'
+PACKAGE='libpam-pwquality'
+
+PATTERN_COMMON="pam_pwquality.so"
+FILE_COMMON="/etc/pam.d/common-password"
+
+PATTERNS_QUALITY=""
+FILE_QUALITY="/etc/security/pwquality.conf"
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
@@ -25,12 +29,23 @@ audit () {
         crit "$PACKAGE is not installed!"
     else
         ok "$PACKAGE is installed"
-        does_pattern_exist_in_file $FILE $PATTERN
+        does_pattern_exist_in_file $FILE_COMMON $PATTERN_COMMON
         if [ $FNRET = 0 ]; then
-            ok "$PATTERN is present in $FILE"
+            ok "$PATTERN_COMMON is present in $FILE_COMMON"
         else
-            crit "$PATTERN is not present in $FILE"
+            crit "$PATTERN_COMMON is not present in $FILE_COMMON"
         fi
+        for PATTERN in $PATTERNS_QUALITY; do
+            OPTION=$(cut -d = -f 1 <<< $PATTERN)
+            PARAM=$(cut -d = -f 2 <<< $PATTERN)
+            PATTERN="$OPTION *= *$PARAM"
+            does_pattern_exist_in_file $FILE_QUALITY $PATTERN
+            if [ $FNRET = 0 ]; then
+                ok "$PATTERN is present in $FILE_QUALITY"
+            else
+                crit "$PATTERN is not present in $FILE_QUALITY"
+            fi
+        done
     fi
 }
 
@@ -48,8 +63,17 @@ apply () {
         ok "$PATTERN is present in $FILE"
     else
         crit "$PATTERN is not present in $FILE"
-        add_line_file_before_pattern $FILE "auth    required    pam_tally.so onerr=fail deny=6 unlock_time=1800" "# Uncomment and edit \/etc\/security\/time.conf if you need to set"
+        add_line_file_before_pattern $FILE "password    requisite           pam_cracklib.so retry=3 minlen=8 difok=3" "# pam-auth-update(8) for details."
     fi 
+}
+
+# This function will create the config file for this check with default values
+create_config() {
+    cat <<EOF
+status=audit
+# Put your custom configuration here
+PATTERNS_QUALITY="^minlen=14 ^dcredit=-1 ^ucredit=-1 ^ocredit=-1 ^lcredit=-1"
+EOF
 }
 
 # This function will check config parameters required

d91fdbf84bcc68c5e644824d6364e1bab52e5a5f
Charles Herlin <charles.herlin@corp.ovh.com>
2019-10-21 14:45:25 +0200

    Add missing tests CUPS, telnet and LDAP
    	new file:   bin/hardening/2.2.4_disable_print_server.sh
    	new file:   bin/hardening/2.3.4_telnet_client_not_installed.sh
    	new file:   bin/hardening/2.3.5_ldap_client_not_installed.sh
    	new file:   tests/hardening/2.2.4_disable_print_server.sh
    	new file:   tests/hardening/2.3.4_telnet_client_not_installed.sh
    	new file:   tests/hardening/2.3.5_ldap_client_not_installed.sh

---
+++ bin/hardening/2.2.4_disable_print_server.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#
+# CIS Debian Hardening
+#
+
+#
+# 2.2.4 Ensure CUPS is not enabled (Scored)
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+HARDENING_LEVEL=3
+DESCRIPTION="Ensure print server (Common Unix Print System) is not enabled."
+HARDENING_EXCEPTION=cups
+
+PACKAGES='libcups2 libcupscgi1 libcupsimage2 libcupsmime1 libcupsppdc1 cups-common cups-client cups-ppdc libcupsfilters1 cups-filters cups'
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+    for PACKAGE in $PACKAGES; do
+        is_pkg_installed $PACKAGE
+        if [ $FNRET = 0 ]; then
+            crit "$PACKAGE is installed!"
+        else
+            ok "$PACKAGE is absent"
+        fi
+    done
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+    for PACKAGE in $PACKAGES; do
+        is_pkg_installed $PACKAGE
+        if [ $FNRET = 0 ]; then
+            crit "$PACKAGE is installed, purging it"
+            apt-get purge $PACKAGE -y
+            apt-get autoremove
+        else
+            ok "$PACKAGE is absent"
+        fi
+    done
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r $CIS_ROOT_DIR/lib/main.sh ]; then
+    . $CIS_ROOT_DIR/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi

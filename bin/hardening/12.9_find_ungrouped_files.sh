413277d7ebb19331a14891af6bbe2a34a8e3253d
Benjamin MONTHOUEL <benjamin.monthouel@corp.ovh.com>
2020-03-30 19:11:07 +0200

    IMP(12.8,12.9): be able to exclude some paths

--- bin/hardening/12.9_find_ungrouped_files.sh
+++ bin/hardening/12.9_find_ungrouped_files.sh
@@ -15,12 +15,17 @@ HARDENING_LEVEL=2
 DESCRIPTION="Find un-grouped files and directories."
 
 GROUP='root'
+EXCLUDED=''
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
     info "Checking if there are ungrouped files"
     FS_NAMES=$(df --local -P | awk {'if (NR!=1) print $6'} )
-    RESULT=$( $SUDO_CMD find $FS_NAMES -xdev -nogroup -print 2>/dev/null)
+    if [ ! -z $EXCLUDED ]; then
+        RESULT=$( $SUDO_CMD find $FS_NAMES -xdev -nogroup -regextype 'egrep' ! -regex "$EXCLUDED" -print 2>/dev/null)
+    else
+        RESULT=$( $SUDO_CMD find $FS_NAMES -xdev -nogroup -print 2>/dev/null)
+    fi
     if [ ! -z "$RESULT" ]; then
         crit "Some ungrouped files are present"
         FORMATTED_RESULT=$(sed "s/ /\n/g" <<< $RESULT | sort | uniq | tr '\n' ' ')

99d63964dcffc212d6fc4117b830c1a6930a940c
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-10-26 14:46:42 +0100

    IMP(6.2.2,6.2.3,6.2.4): add purposely failing tests

--- tests/hardening/6.2.3_remove_legacy_shadow_entries.sh
+++ tests/hardening/6.2.3_remove_legacy_shadow_entries.sh
@@ -6,5 +6,25 @@ test_audit() {
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    local test_user="testetcshadowusr"
+
+    describe Tests purposely failing 
+    useradd $test_user
+    sed -i "s/$test_user:/+:$test_user:/" /etc/shadow
+    register_test retvalshouldbe 1
+    register_test contain "Some accounts have a legacy password entry"
+    run noncompliant /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    describe correcting situation
+    sed  -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+    /opt/debian-cis/bin/hardening/"${script}".sh --apply || true
+
+    describe Checking resolved state
+    register_test retvalshouldbe 0
+    register_test contain "All accounts have a valid password entry format"
+    run resolved /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    # cleanup
+    userdel $test_user
+
 }

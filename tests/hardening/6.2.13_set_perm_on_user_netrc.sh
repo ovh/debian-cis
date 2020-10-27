3bf1067a2494b7ff35a1f098a16fc6c72ff9b7f6
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-10-27 11:04:55 +0100

    IMP(6.2.12,6.2.13): add purposely failing tests

--- tests/hardening/6.2.13_set_perm_on_user_netrc.sh
+++ tests/hardening/6.2.13_set_perm_on_user_netrc.sh
@@ -6,5 +6,26 @@ test_audit() {
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    local test_user="testnetrcuser"
+    local test_file=".netrc"
+
+    describe Tests purposely failing 
+    useradd --create-home $test_user
+    touch /home/$test_user/$test_file
+    chmod 777 /home/$test_user/$test_file
+    register_test retvalshouldbe 1
+    register_test contain "permissions were not set to"
+    run noncompliant /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    describe correcting situation
+    sed  -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+    /opt/debian-cis/bin/hardening/"${script}".sh --apply || true
+
+    describe Checking resolved state
+    register_test retvalshouldbe 0
+    register_test contain "$test_file has correct permissions"
+    run resolved /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    # cleanup
+    userdel -r $test_user
 }

5ea053a5023094f572ec495c5d3fc2bd86901ae0
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-10-27 11:04:55 +0100

    IMP(6.2.12,6.2.13): add purposely failing tests

--- tests/hardening/6.2.12_find_user_netrc_files.sh
+++ tests/hardening/6.2.12_find_user_netrc_files.sh
@@ -6,5 +6,16 @@ test_audit() {
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    local test_user="testnetrcuser"
+    local test_file=".netrc"
+
+    describe Tests purposely failing 
+    useradd --create-home $test_user
+    touch /home/$test_user/$test_file
+    register_test retvalshouldbe 1
+    register_test contain "$test_file present"
+    run noncompliant /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    # cleanup
+    userdel -r $test_user
 }

1fd132b8c6ab83feb1d00a548c5d77f26281205e
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-10-27 11:24:40 +0100

    IMP(6.2.18,6.2.19,6.2.20): add purposely failing tests

--- tests/hardening/6.2.18_check_duplicate_username.sh
+++ tests/hardening/6.2.18_check_duplicate_username.sh
@@ -6,5 +6,18 @@ test_audit() {
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    local test_user="testduplicateuser"
+    local dir="/etc/passwd"
+
+    describe Tests purposely failing 
+    useradd $test_user
+    temp=$(tail -1 $dir)
+    echo $temp >> $dir
+    register_test retvalshouldbe 1
+    register_test contain "Duplicate username"
+    run noncompliant /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    # cleanup
+    sed -i '$ d' $dir
+    userdel $test_user
 }

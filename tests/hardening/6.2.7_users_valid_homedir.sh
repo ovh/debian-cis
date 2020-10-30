b266982a3c5fe4e9e02c0f84066edca10cc4327d
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-10-30 16:01:18 +0100

    ADD(6.2.7): add check mysteriously deleted during renaming

---
+++ tests/hardening/6.2.7_users_valid_homedir.sh
@@ -0,0 +1,18 @@
+# run-shellcheck
+test_audit() {
+    describe Running on blank host
+    register_test retvalshouldbe 0
+    dismiss_count_for_test
+    # shellcheck disable=2154
+    run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    local test_user="userwithouthome"
+    useradd $test_user
+    describe Tests purposely failing
+    register_test retvalshouldbe 1
+    register_test contain "does not exist."
+    run noncompliant /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+
+    # cleanup
+    userdel $test_user
+}

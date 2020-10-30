1ce6bf79306e581f66121065ea5e9174d8fb2ed4
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-10-30 09:40:28 +0100

    IMP(5.4.3): add purposely failing test

--- tests/hardening/5.4.3_default_root_group.sh
+++ tests/hardening/5.4.3_default_root_group.sh
@@ -6,5 +6,13 @@ test_audit() {
     # shellcheck disable=2154
     run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-    # TODO fill comprehensive tests
+    usermod -g 1 root
+    describe Correcting situation
+    sed -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+    /opt/debian-cis/bin/hardening/"${script}".sh || true
+
+    describe Checking resolved state
+    register_test retvalshouldbe 0
+    register_test contain "[ OK ] Root group has GID 0"
+    run resolved /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 }

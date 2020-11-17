263ff54f46cc35ad39ae9bf4291794d0d13f0fa3
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-11-17 12:56:10 +0100

    fixup! IMP(4.5): rename to 1.6.1.2 improve test

--- tests/hardening/1.6.2.1_enable_apparmor.sh
+++ tests/hardening/1.6.2.1_enable_apparmor.sh
@@ -9,6 +9,13 @@ test_audit() {
         # shellcheck disable=2154
         run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
 
-        # TODO fill comprehensive tests
+        describe correcting situation
+        sed  -i 's/audit/enabled/' /opt/debian-cis/etc/conf.d/"${script}".cfg
+        /opt/debian-cis/bin/hardening/"${script}".sh --apply || true
+
+        describe Checking resolved state
+        register_test retvalshouldbe 0
+        register_test contain "is configured"
+        run resolved /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
     fi
 }

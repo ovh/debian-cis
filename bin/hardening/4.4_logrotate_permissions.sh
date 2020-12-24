e2ad0a5dccdf3267c2cd438cf4329d4c0498e597
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2020-12-24 10:31:47 +0100

    ADD(4.4): add logrotate permissions checking

--- bin/hardening/4.4_logrotate_permissions.sh
+++ bin/hardening/4.4_logrotate_permissions.sh
@@ -17,14 +17,45 @@ HARDENING_LEVEL=3
 # shellcheck disable=2034
 DESCRIPTION="Configure logrotate to assign appropriate permissions."
 
+FILE="/etc/logrotate.conf"
+PATTERN="^\s*create\s+\S+"
+PERMISSIONS=0640
+
 # This function will be called if the script status is on enabled / audit mode
 audit() {
-    :
+    does_pattern_exist_in_file "$FILE" "$PATTERN"
+    if [ "$FNRET" != 0 ]; then
+        crit "Logrotate permissions are not configured"
+    else
+        if grep -E "$PATTERN" "$FILE" | grep -E -v "\s(0)?[0-6][04]0\s"; then
+            crit "Logrotate permissions are not set to $PERMISSIONS"
+        else
+            ok "Logrotate permissions are well configured"
+        fi
+    fi
 }
 
 # This function will be called if the script status is on enabled mode
 apply() {
-    :
+    does_pattern_exist_in_file "$FILE" "$PATTERN"
+    if [ "$FNRET" != 0 ]; then
+        warn "Logrotate permissions are not configured, fixing it"
+        add_end_of_file "$FILE" "create $PERMISSIONS root utmp"
+    else
+        RESULT=$(grep -E "$PATTERN" "$FILE" | grep -E -v "\s(0)?[0-6][04]0\s")
+        if [[ -n "$RESULT" ]]; then
+            warn "Logrotate permissions are not set to $PERMISSIONS, fixing it"
+            d_IFS=$IFS
+            c_IFS=$'\n'
+            IFS=$c_IFS
+            for SOURCE in $RESULT; do
+                replace_in_file "$FILE" "$SOURCE" "create $PERMISSIONS root utmp"
+            done
+            IFS=$d_IFS
+        else
+            ok "Logrotate permissions are well configured"
+        fi
+    fi
 }
 
 # This function will check config parameters required

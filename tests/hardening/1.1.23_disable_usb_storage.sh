ed1baa724edbfca616424d27fb92ddf6cfc6bb28
Thibault Ayanides <thibault.ayanides@ovhcloud.com>
2021-01-21 11:21:18 +0100

    IMP: mark some checks as useless

--- tests/hardening/1.1.23_disable_usb_storage.sh
+++ tests/hardening/1.1.23_disable_usb_storage.sh
@@ -1,12 +1,15 @@
 # shellcheck shell=bash
 # run-shellcheck
 test_audit() {
-    describe Running on blank host
-    register_test retvalshouldbe 0
-    dismiss_count_for_test
-    # shellcheck disable=2154
-    run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
-
+    if [ -f "/.dockerenv" ]; then
+        skip "SKIPPED on docker"
+    else
+        describe Running on blank host
+        register_test retvalshouldbe 0
+        dismiss_count_for_test
+        # shellcheck disable=2154
+        run blank /opt/debian-cis/bin/hardening/"${script}".sh --audit-all
+    fi
     ##################################################################
     # For this test, we only check that it runs properly on a blank  #
     # host, and we check root/sudo consistency. But, we don't test   #

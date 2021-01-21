b2e9aa7c71161de26191ccb1a644a6a46ac89a7f
Jeremy Denoun <jeremy.denoun@iguanesolutions.com>
2021-01-21 15:44:04 +0100

    use fix shfmt version

--- bin/hardening/99.5.2.3_ssh_disable_features.sh
+++ bin/hardening/99.5.2.3_ssh_disable_features.sh
@@ -22,77 +22,77 @@ OPTIONS='AllowAgentForwarding=no AllowTcpForwarding=no AllowStreamLocalForwardin
 
 # This function will be called if the script status is on enabled / audit mode
 audit() {
-	is_pkg_installed "$PACKAGE"
-	if [ "$FNRET" != 0 ]; then
-		ok "$PACKAGE is not installed!"
-	else
-		ok "$PACKAGE is installed"
-		for SSH_OPTION in $OPTIONS; do
-			SSH_PARAM=$(echo "$SSH_OPTION" | cut -d= -f 1)
-			SSH_VALUE=$(echo "$SSH_OPTION" | cut -d= -f 2)
-			PATTERN="^${SSH_PARAM}[[:space:]]*$SSH_VALUE"
-			does_pattern_exist_in_file_nocase "$FILE" "$PATTERN"
-			if [ "$FNRET" = 0 ]; then
-				ok "$PATTERN is present in $FILE"
-			else
-				crit "$PATTERN is not present in $FILE"
-			fi
-		done
-	fi
+    is_pkg_installed "$PACKAGE"
+    if [ "$FNRET" != 0 ]; then
+        ok "$PACKAGE is not installed!"
+    else
+        ok "$PACKAGE is installed"
+        for SSH_OPTION in $OPTIONS; do
+            SSH_PARAM=$(echo "$SSH_OPTION" | cut -d= -f 1)
+            SSH_VALUE=$(echo "$SSH_OPTION" | cut -d= -f 2)
+            PATTERN="^${SSH_PARAM}[[:space:]]*$SSH_VALUE"
+            does_pattern_exist_in_file_nocase "$FILE" "$PATTERN"
+            if [ "$FNRET" = 0 ]; then
+                ok "$PATTERN is present in $FILE"
+            else
+                crit "$PATTERN is not present in $FILE"
+            fi
+        done
+    fi
 }
 
 # This function will be called if the script status is on enabled mode
 apply() {
-	is_pkg_installed "$PACKAGE"
-	if [ "$FNRET" = 0 ]; then
-		ok "$PACKAGE is installed"
-	else
-		crit "$PACKAGE is absent, installing it"
-		apt_install "$PACKAGE"
-	fi
-	for SSH_OPTION in $OPTIONS; do
-		SSH_PARAM=$(echo "$SSH_OPTION" | cut -d= -f 1)
-		SSH_VALUE=$(echo "$SSH_OPTION" | cut -d= -f 2)
-		PATTERN="^${SSH_PARAM}[[:space:]]*$SSH_VALUE"
-		does_pattern_exist_in_file_nocase "$FILE" "$PATTERN"
-		if [ "$FNRET" = 0 ]; then
-			ok "$PATTERN is present in $FILE"
-		else
-			warn "$PATTERN is not present in $FILE, adding it"
-			does_pattern_exist_in_file_nocase "$FILE" "^${SSH_PARAM}"
-			if [ "$FNRET" != 0 ]; then
-				add_end_of_file "$FILE" "$SSH_PARAM $SSH_VALUE"
-			else
-				info "Parameter $SSH_PARAM is present but with the wrong value -- Fixing"
-				replace_in_file "$FILE" "^${SSH_PARAM}[[:space:]]*.*" "$SSH_PARAM $SSH_VALUE"
-			fi
-			/etc/init.d/ssh reload >/dev/null 2>&1
-		fi
-	done
+    is_pkg_installed "$PACKAGE"
+    if [ "$FNRET" = 0 ]; then
+        ok "$PACKAGE is installed"
+    else
+        crit "$PACKAGE is absent, installing it"
+        apt_install "$PACKAGE"
+    fi
+    for SSH_OPTION in $OPTIONS; do
+        SSH_PARAM=$(echo "$SSH_OPTION" | cut -d= -f 1)
+        SSH_VALUE=$(echo "$SSH_OPTION" | cut -d= -f 2)
+        PATTERN="^${SSH_PARAM}[[:space:]]*$SSH_VALUE"
+        does_pattern_exist_in_file_nocase "$FILE" "$PATTERN"
+        if [ "$FNRET" = 0 ]; then
+            ok "$PATTERN is present in $FILE"
+        else
+            warn "$PATTERN is not present in $FILE, adding it"
+            does_pattern_exist_in_file_nocase "$FILE" "^${SSH_PARAM}"
+            if [ "$FNRET" != 0 ]; then
+                add_end_of_file "$FILE" "$SSH_PARAM $SSH_VALUE"
+            else
+                info "Parameter $SSH_PARAM is present but with the wrong value -- Fixing"
+                replace_in_file "$FILE" "^${SSH_PARAM}[[:space:]]*.*" "$SSH_PARAM $SSH_VALUE"
+            fi
+            /etc/init.d/ssh reload >/dev/null 2>&1
+        fi
+    done
 
 }
 
 # This function will check config parameters required
 check_config() {
-	:
+    :
 }
 
 # Source Root Dir Parameter
 if [ -r /etc/default/cis-hardening ]; then
-	# shellcheck source=../../debian/default
-	. /etc/default/cis-hardening
+    # shellcheck source=../../debian/default
+    . /etc/default/cis-hardening
 fi
 if [ -z "$CIS_ROOT_DIR" ]; then
-	echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
-	echo "Cannot source CIS_ROOT_DIR variable, aborting."
-	exit 128
+    echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+    echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
 fi
 
 # Main function, will call the proper functions given the configuration (audit, enabled, disabled)
 if [ -r "$CIS_ROOT_DIR"/lib/main.sh ]; then
-	# shellcheck source=../../lib/main.sh
-	. "$CIS_ROOT_DIR"/lib/main.sh
+    # shellcheck source=../../lib/main.sh
+    . "$CIS_ROOT_DIR"/lib/main.sh
 else
-	echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
-	exit 128
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
 fi

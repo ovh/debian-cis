6123a56653472bd13614c6c7c53619920b252c32
damcav35 <51324122+damcav35@users.noreply.github.com>
2025-07-03 09:27:09 +0200

    fix: update record_mac_edit.sh to use apparmor instead of selinux (#262)
    Update record_mac_edit.sh to be compliant with debian11 and debian12 CIS recommendations.
    
    fix issue #195
    
    Co-authored-by: Damien Cavagnini <damien.cavagnini@corp.ovh.com>

--- tests/hardening/record_mac_edit.sh
+++ tests/hardening/record_mac_edit.sh
@@ -2,8 +2,7 @@
 # run-shellcheck
 test_audit() {
     describe Running on blank host
-    register_test retvalshouldbe 0
-    dismiss_count_for_test
+    register_test retvalshouldbe 1
     # shellcheck disable=2154
     run blank "${CIS_CHECKS_DIR}/${script}.sh" --audit-all
 
@@ -13,6 +12,6 @@ test_audit() {
 
     describe Checking resolved state
     register_test retvalshouldbe 0
-    register_test contain "[ OK ] -w /etc/selinux/ -p wa -k MAC-policy is present in /etc/audit/rules.d/audit.rules"
+    register_test contain "[ OK ] -w /etc/apparmor/ -p wa -k MAC-policy is present in /etc/audit/rules.d/audit.rules"
     run resolved "${CIS_CHECKS_DIR}/${script}.sh" --audit-all
 }

fbeac63e349cb2dcd0f840c31dd51342400ff5d8
damcav35 <51324122+damcav35@users.noreply.github.com>
2025-07-30 15:03:29 +0200

    fix: shellcheck precommit (#276)
    Co-authored-by: damien cavagnini <damien.cavagnini@corp.ovh.com>

--- hooks/shellcheck.sh
+++ hooks/shellcheck.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+issues=0
+
 for script in "$@"; do
     /usr/bin/shellcheck --exclude=SC2317 --color=always --shell=bash -x --source-path=SCRIPTDIR "$script"
+    [ $? -eq 0 ] || issues=$(($issues + 1))
 done
 
-exit 0
+if [ "$issues" -gt 0 ]; then
+    exit 1
+else
+    exit 0
+fi

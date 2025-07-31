c3506fdd476737d51093064660ac25781277047e
damien cavagnini <damien.cavagnini@corp.ovh.com>
2025-07-31 12:16:03 +0200

    feat: add debian12 scripts
    - aide_daliy_check				-> 6.1.2
    - journald_is_enabled.sh 			-> 6.2.1.1.1
    - systemd_journald_remote_is_installed.sh 	-> 6.2.1.2.1
    - systemd_journal_upload_is_enabled.sh -	-> 6.2.1.2.3
    - systemd_journal_remote_is_disabled.sh 	-> 6.2.1.2.4

---
+++ tests/hardening/systemd_journal_remote_is_disabled.sh
@@ -0,0 +1,11 @@
+# shellcheck shell=bash
+# run-shellcheck
+test_audit() {
+
+    # not much to test here, we are running in a container, we wont check service state
+    describe Checking blank host
+    register_test retvalshouldbe 0
+    # shellcheck disable=2154
+    run blank "${CIS_CHECKS_DIR}/${script}.sh" --audit-all
+
+}
